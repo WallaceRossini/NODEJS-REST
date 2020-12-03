77428026186cbfbe4d770e02c791a0ac63b3c555
Wallace Rossini <rossiniwallace@gmail.com>
2020-12-03 11:35:22 -0300

    Rota para criar atendimento

--- src/controllers/atendimentos.js
+++ src/controllers/atendimentos.js
@@ -1,5 +1,7 @@
 module.exports = app => {
 
-  app.get('/atendimentos', (request, response) => { response.send('Você está na rota de atendimentos')});
-  
+  app.get('/atendimentos', (request, response) =>  response.send('Você está na rota de atendimentos e está realizando um GET'));
+
+  app.post('/atendimentos', (request, response) => response.send('Você está na rota de atendimentos e está realizando um POST'));
+
 }
