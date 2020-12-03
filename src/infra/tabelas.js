f21ce3cceb8f50fb69234766ce483d215583d953
Wallace Rossini <rossiniwallace@gmail.com>
2020-12-03 12:29:11 -0300

    Adicionando data no atendimento

--- src/infra/tabelas.js
+++ src/infra/tabelas.js
@@ -11,6 +11,8 @@ class Tabelas {
       cliente varchar(50) not null,
       pet varchar(20),
       servico varchar(20) not null,
+      data datetime not null,
+      dataCriacao datetime not null,
       status varchar(20) not null,
       observacoes text,
       primary key (id))`;
--- src/models/atendimentos.js
+++ src/models/atendimentos.js
@@ -1,7 +1,13 @@
+const moment = require('moment');
 const conexao = require('../infra/conexao');
 class Atendimento {
 
   adiciona(atendimento){
+
+      const dataCriacao = new Date;
+      atendimento.data = moment(atendimento.data,'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+      atendimento = {...atendimento,dataCriacao};
+
       const sql = 'insert into Atendimentos set ?';
 
       conexao.query(sql,atendimento, (erro,resultado) => {
