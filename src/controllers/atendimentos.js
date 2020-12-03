81c4036bcc33d4d6af16753b88b7050d7f3d1995
Wallace Rossini <rossiniwallace@gmail.com>
2020-12-03 11:44:27 -0300

    Enviar dados relativos ao pagamento

--- src/controllers/atendimentos.js
+++ src/controllers/atendimentos.js
@@ -2,6 +2,11 @@ module.exports = app => {
 
   app.get('/atendimentos', (request, response) =>  response.send('Você está na rota de atendimentos e está realizando um GET'));
 
-  app.post('/atendimentos', (request, response) => response.send('Você está na rota de atendimentos e está realizando um POST'));
+  app.post('/atendimentos', (request, response) => {
+
+    console.log(request.body);
+    
+    response.send('Você está na rota de atendimentos e está realizando um POST')
+  });
 
 }
