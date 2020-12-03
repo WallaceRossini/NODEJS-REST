9e1ddc1200f189f000a3ddbfef5309f7e517a8b6
Wallace Rossini <rossiniwallace@gmail.com>
2020-12-03 12:44:51 -0300

    Aplicando validações de entrada

--- src/models/atendimentos.js
+++ src/models/atendimentos.js
@@ -8,12 +8,37 @@ class Atendimento {
       atendimento.data = moment(atendimento.data,'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
       atendimento = {...atendimento,dataCriacao};
 
-      const sql = 'insert into Atendimentos set ?';
+      const dataEhValida = moment(atendimento.data).isSameOrAfter(dataCriacao);
 
-      conexao.query(sql,atendimento, (erro,resultado) => {
-        if(erro) response.status(400).json(erro);
-        else response.status(201).json(resultado);
-      })
+      const clienteEhValido = atendimento.cliente.length >=5;
+
+      const validacoes = [
+        {
+          nome:'data',
+          valido: dataEhValida,
+          mensagem: 'Data deve ser maior ou igual a data atual'
+        },
+        {
+          nome:'cliente',
+          valido: clienteEhValido,
+          mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+        }
+      ];
+
+      const erros = validacoes.filter(campo => !campo.valido);
+
+      const existemErros = erros.length;
+      
+      if(existemErros){
+        response.status(400).json(erros);
+      }else{
+        const sql = 'insert into Atendimentos set ?';
+
+        conexao.query(sql,atendimento, (erro,resultado) => {
+          if(erro) response.status(400).json(erro);
+          else response.status(201).json(resultado);
+        })
+      }
   }
 }
 
